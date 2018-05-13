089d9a1987887db6fe5675d2f4544057d38968e4
prantoran <prantoran@gmail.com>
2018-05-13 14:33:27 +0600

    user-service dbg

--- user-service/handler.go
+++ user-service/handler.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"log"
-
 	pb "github.com/prantoran/evshippy/user-service/proto/user"
-	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/net/context"
 )
 
@@ -32,34 +29,15 @@ func (srv *service) GetAll(ctx context.Context, req *pb.Request, res *pb.Respons
 }
 
 func (srv *service) Auth(ctx context.Context, req *pb.User, res *pb.Token) error {
-	log.Println("Logging in with:", req.Email, req.Password)
-	user, err := srv.repo.GetByEmail(req.Email)
-	log.Println(user)
+	_, err := srv.repo.GetByEmailAndPassword(req)
 	if err != nil {
 		return err
 	}
-
-	// Compares our given password against the hashed password
-	// stored in the database
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
-		return err
-	}
-
-	token, err := srv.tokenService.Encode(user)
-	if err != nil {
-		return err
-	}
-	res.Token = token
+	res.Token = "testingabc"
 	return nil
 }
 
 func (srv *service) Create(ctx context.Context, req *pb.User, res *pb.Response) error {
-	// Generates a hashed version of our password
-	hashedPass, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
-	if err != nil {
-		return err
-	}
-	req.Password = string(hashedPass)
 	if err := srv.repo.Create(req); err != nil {
 		return err
 	}
